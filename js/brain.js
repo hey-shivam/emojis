3a8c21cae46b38610a38845a520f6861456bdff4
Shivam Kumar <hey.shivam39@gmail.com>
2021-02-06 20:28:20 +0530

    add some emoji

---
+++ js/brain.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('emoji-btn');
+const emojis =['😀','😃','😆','😉','😇','😎','😫','😳','😝','😙','😅',"😂",
+"😀","😊","😗","😛","😏","😊","😗","😛","😏","😴","😧","💗", "😍", "😊", "❤️", "😁", "😕", "😪", "🎅"];
+
+btn.addEventListener('mouseover',() => {
+    btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];  
+});
